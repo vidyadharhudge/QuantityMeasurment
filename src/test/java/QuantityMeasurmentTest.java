e44e619ebb1456279ccb49205ad1d605fcb7ad9a
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-05-15 15:15:57 +0530

    Added:-T.c 1.3:-Given Refrencee Object Equal Should Return True

--- src/test/java/QuantityMeasurmentTest.java
+++ src/test/java/QuantityMeasurmentTest.java
@@ -14,17 +14,25 @@ public class QuantityMeasurmentTest
     }
 
     @Test
-    public void givenZeroFeetAndZeroFeetValue_WhenEual_ThenShouldReturnTrue()
+    public void givenZeroFeetAndZeroFeetValue_WhenEqual_ThenShouldReturnTrue()
     {
         double value1=qunatityMeasurment.getFeet(0.0);
         double value2=qunatityMeasurment.getFeet(0.0);
         Assert.assertEquals(value1,value2,0.0);
     }
-    
+
     @Test
     public void givenFeetValue_WhenNull_ThenShouldReturnFalse()
     {
        Assert.assertFalse(qunatityMeasurment.equals(null));
     }
+
+    @Test
+    public void givenRefrenceObject_WhenEqual_ThenShouldReturnTrue()
+    {
+       QunatityMeasurment qunatityMeasurment1=new QunatityMeasurment();
+       Assert.assertEquals(qunatityMeasurment,qunatityMeasurment1);
+    }
+
 
 }
